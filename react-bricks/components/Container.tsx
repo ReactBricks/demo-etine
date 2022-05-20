f0a162c6ec3d81a1628026a63b324d3acc61f0ca
Giorgio Tognoli <giorgio@f2.net>
2022-05-20 12:25:30 +0200

    updated react-bricks

--- react-bricks/components/Container.tsx
+++ react-bricks/components/Container.tsx
@@ -6,7 +6,9 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ className, children }) => {
-  return <div className={clsx('container py-16 md:py-24 2xl:max-w-7xl', className)}>{children}</div>
+  return (
+    <div className={clsx('container py-16 md:py-24 2xl:max-w-7xl px-6', className)}>{children}</div>
+  )
 }
 
 export default Container
